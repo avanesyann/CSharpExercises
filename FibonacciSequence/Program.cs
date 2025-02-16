c206f23456b67cbd7e80035d62c7434090777a55
Narek Avanesyan <avanesyan12345@gmail.com>
2025-02-17 01:20:22 +0400

    Initial push

---
+++ FibonacciSequence/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace FibonacciSequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write a number: ");
+
+            if (int.TryParse(Console.ReadLine(), out int userInput))
+            {
+                Console.WriteLine($"Fibonacci({userInput}) = {Fibonacci(userInput)}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input.");
+            }
+        }
+        static long Fibonacci(int n)
+        {
+            if (n <= 0) return 0;
+            if (n == 1) return 1;
+
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
