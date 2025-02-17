ff765b18f7f1601d12154aac09302e8126f2e32a
Narek Avanesyan <avanesyan12345@gmail.com>
2025-02-17 23:19:45 +0400

    Add GradesTracker. Practicing indexers.

---
+++ GradesTracker/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace GradesTracker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Simple test
+            StudentGrades grades = new StudentGrades();
+            grades[0] = 95;
+            grades[1] = 75;
+            grades[2] = 60;
+            grades[3] = 65;
+
+            Console.WriteLine(grades[0]);
+            Console.WriteLine(grades[1]);
+            Console.WriteLine(grades[2]);
+            Console.WriteLine(grades[3]);
+        }
+    }
+}
---
+++ GradesTracker/StudentGrades.cs
@@ -0,0 +1,39 @@
+﻿namespace GradesTracker
+{
+    public class StudentGrades
+    {
+        private List<int> _students;
+        public int Count => _students.Count;
+
+        public StudentGrades()
+        {
+            _students = new List<int>();
+        }
+
+        public int this[int key]
+        {
+            get
+            {
+                if (key < 0 || key >= _students.Count)
+                throw new IndexOutOfRangeException("Invalid index.");
+
+                return _students[key];
+            }
+            set 
+            {
+                if (key < 0)
+                    throw new IndexOutOfRangeException("Index cannot be negative.");
+
+                while (_students.Count <= key)
+                    _students.Add(0);
+
+                _students[key] = value; 
+            }
+        }
+
+        public int AverageGrade()
+        {
+            return Count != 0 ? _students.Sum() / Count : 0;
+        }
+    }
+}
