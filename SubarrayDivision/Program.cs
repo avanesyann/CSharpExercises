22adf9d3a3955d5645bc378d076a02288e7f059a
Narek Avanesyan <avanesyan12345@gmail.com>
2025-02-17 15:38:50 +0400

    Add simple unit testing and value checking

--- SubarrayDivision/Program.cs
+++ SubarrayDivision/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace SubarrayDivision
+﻿using System.Globalization;
+
+namespace SubarrayDivision
 {
     internal class Program
     {
@@ -12,19 +14,32 @@
         }
         static int Birthday(List<int> chocolatePieces, int day, int month)
         {
+            if (chocolatePieces == null || chocolatePieces.Count == 0)
+                throw new ArgumentException("The list of numbers cannot be null or empty.");
+
+            if (month <= 0 || month > chocolatePieces.Count)
+                throw new ArgumentException("Month value must be greater than 0 and less than or equal to the list size.");
+
+            if (day <= 0)
+            {
+                throw new ArgumentException("Day value must be positive integer.");
+            }
+
             int count = 0;
             int sum = 0;
 
+            // iterate through the list while maintaining a sliding window of size 'month'
             for (int i = 0; i < chocolatePieces.Count; i++)
             {
                 sum += chocolatePieces[i];
 
+                // once we reach the required windows size
                 if (i >= month - 1)
                 {
-                    if (sum == day)
+                    if (sum == day) // check if sum matches the required value
                         count++;
 
-                    sum -= chocolatePieces[i - (month - 1)];
+                    sum -= chocolatePieces[i - (month - 1)];    // slide window forward
                 }
             }
 
