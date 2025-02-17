6e39cdf8ec6afcc27d3b858c36e084c8cfc6c3db
Narek Avanesyan <avanesyan12345@gmail.com>
2025-02-17 15:35:26 +0400

    Implement slidingn window algorithm for subarray sum

---
+++ SubarrayDivision/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace SubarrayDivision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> chocolate = new List<int>() { 1, 2, 1, 3, 2 };
+            int bars = 2;
+            int sum = 3;
+
+            Console.WriteLine(Birthday(chocolate, sum, bars));
+        }
+        static int Birthday(List<int> chocolatePieces, int day, int month)
+        {
+            int count = 0;
+            int sum = 0;
+
+            for (int i = 0; i < chocolatePieces.Count; i++)
+            {
+                sum += chocolatePieces[i];
+
+                if (i >= month - 1)
+                {
+                    if (sum == day)
+                        count++;
+
+                    sum -= chocolatePieces[i - (month - 1)];
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ SubarrayDivision/README.md
@@ -0,0 +1,2 @@
+# Sliding Window Subarray Sum 
+This program finds the number of contiguous subarrays (of a given length) whose sum equals a target value. 
