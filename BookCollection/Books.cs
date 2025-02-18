56e5fb2e32cc62eefdab97bca090873fb431ca62
Narek Avanesyan <avanesyan12345@gmail.com>
2025-02-18 21:16:38 +0400

    Add new project 'BookCollection'

---
+++ BookCollection/Books.cs
@@ -0,0 +1,61 @@
+﻿namespace BookCollection
+{
+    public class Books
+    {
+        private List<(string isbn, string title)> _books;
+        public int Count => _books.Count;
+
+        public Books()
+        {
+            _books = new List<(string isbn, string title)>();
+        }
+
+        public string this[string isbn]
+        {
+            get
+            {
+                foreach (var book in _books)
+                {
+                    if (book.isbn == isbn)
+                        return book.title;
+                }
+
+                throw new KeyNotFoundException($"ISBN '{isbn}' not found.");
+            }
+            set
+            {
+                for (int i = 0; i < Count; i++)
+                {
+                    if (_books[i].isbn == isbn)
+                    {
+                        _books[i] = (isbn, value);
+                        return;
+                    }
+                }
+
+                throw new KeyNotFoundException($"ISBN '{isbn}' not found.");
+            }
+        }
+        public string this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= Count)
+                    throw new ArgumentOutOfRangeException($"Index {index} is out of range.");
+
+                return _books[index].title;
+            }
+        }
+
+        public void AddBook(string isbn, string title)
+        {
+            foreach (var book in _books)
+            {
+                if (book.isbn == isbn)
+                    throw new InvalidOperationException($"ISBN '{isbn}' already exists.");
+            }
+            _books.Add((isbn, title));
+        }
+
+    }
+}
---
+++ BookCollection/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace BookCollection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Books library = new Books();
+            library.AddBook("978-0134685997", "The Great Gatsby");
+            library.AddBook("978-0154686998", "In Search of Lost Time");
+            library.AddBook("000-0000000001", "The Epic of Gilgamesh");
+            library.AddBook("978-0154616998", "One Hundred Years of Solitude");
+            library.AddBook("978-0158195468", "Lolita");
+            library.AddBook("978-0119935713", "Anna Karenina");
+            library.AddBook("978-0112348385", "War and Peace");
+            library.AddBook("978-0155812844", "Flowers For Algernon");
+            library.AddBook("978-0112388345", "To Kill a Mockingbird");
+
+            library["978-0158195468"] = "The Old Man and The Sea";
+
+            Console.WriteLine(library[6]);
+            Console.WriteLine(library[7]);
+            Console.WriteLine(library[8]);
+            Console.WriteLine(library[4]);
+        }
+    }
+}
