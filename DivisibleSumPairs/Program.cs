9503e6d25f3978b47010080e16e8bbe5c116d462
Narek Avanesyan <narek.avanesyan.s@sportgenerate.com>
2025-02-19 23:38:05 +0400

    add DivisibleSumPairs project

---
+++ DivisibleSumPairs/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace DivisibleSumPairs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6 };
+            int k = 5;
+
+            DivisibleSumPairs(numbers, k);
+        }
+
+
+        static int DivisibleSumPairs(List<int> numbers, int k)
+        {
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                for (int j = numbers.Count - 1; j < numbers.Count; j++)
+                {
+                    Console.WriteLine($"[{i}; {j}]");
+                }
+            }
+            return 0;
+        }
+    }
+}
