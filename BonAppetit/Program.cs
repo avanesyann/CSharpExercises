739669ef0098d173d1aeba548dcf4958a11f5fee
Narek Avanesyan <avanesyan12345@gmail.com>
2025-02-20 23:58:54 +0400

    Split the bill.

---
+++ BonAppetit/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace BonAppetit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Simple test
+
+            List<int> bill = new List<int>() { 3, 10, 2, 9 };
+            int annaDidntEat = 1;
+            int charged = 7;
+
+            BonAppetit(bill, annaDidntEat, charged);
+        }
+
+        static void BonAppetit(List<int> bill, int skippedItemIndex, int chargedAmount)
+        {
+            int totalCost = bill.Sum();
+            int actualCharge = (totalCost - bill[skippedItemIndex]) / 2;
+
+            if (chargedAmount == actualCharge)
+                Console.WriteLine("Bon Appetit");
+            else
+                Console.WriteLine(chargedAmount - actualCharge);
+        }
+    }
+}
---
+++ BonAppetit/README.md
@@ -0,0 +1,7 @@
+# Bon Appetit 
+
+This program calculates the correct amount Anna should pay after skipping an item from a shared bill.
+
+## Usage (feel free to change the values)
+```csharp
+BonAppetit(new List<int>() { 3, 10, 2, 9 }, 1, 12);
