6e0152d9132ed0fd2b73517883400335524f5b89
Narek Avanesyan <avanesyan12345@gmail.com>
2025-02-21 23:55:10 +0400

    Add HurdleRace

---
+++ HurdleRace/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace HurdleRace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(HurdleRace(5, new List<int> { 4, 6, 5, 3, 7, 1, 2}));
+        }
+
+        static int HurdleRace(int k, List<int> height)
+        {
+            int maxHurdle = height.Max();
+
+            if (k >= maxHurdle)
+                return 0;
+
+            return maxHurdle - k;
+        }
+    }
+}
