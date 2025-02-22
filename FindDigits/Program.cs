3585eb5e0cd11396e3c220d22bd578a3232f7f4b
Narek Avanesyan <avanesyan12345@gmail.com>
2025-02-22 13:51:43 +0400

    Update FindDigits project.

--- FindDigits/Program.cs
+++ FindDigits/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(FindDigits(124));
+            Console.WriteLine(FindDigits(1012));
+            Console.WriteLine(FindDigits(54));
+        }
+
+        public static int FindDigits(int n)
+        {
+            int number = Math.Abs(n);
+            int countDivisors = 0;
+
+            while (number > 0)
+            {
+                int digit = number % 10;
+                if (digit != 0 && n % digit == 0)
+                    countDivisors++;
+
+                number /= 10;
+            }
+
+            return countDivisors;
         }
     }
 }
---
+++ FindDigits/README.md
@@ -0,0 +1,1 @@
+# For each digit that makes up the integer determine whether it is a divisor. 
