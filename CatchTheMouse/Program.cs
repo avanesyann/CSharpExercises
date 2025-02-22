58a9d0f38abe3f8f18a6c240ff030013adee2e2e
Narek Avanesyan <avanesyan12345@gmail.com>
2025-02-22 10:43:17 +0400

    Merged local and remote changes, adding two new projects.

---
+++ CatchTheMouse/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace CatchTheMouse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CatAndMouse(2, 1, 3));
+        }
+
+        static string CatAndMouse(int x, int y, int z)
+        {
+            int catA = Math.Abs(z - x);
+            int catB = Math.Abs(z - y);
+            if (catA > catB)
+                return "Cat B";
+            if (catA < catB)
+                return "Cat A";
+
+            return "Mouse C";
+        }
+    }
+}
