3901ec88c8667bb6af3387f3a0cad9142d6e2f98
Narek Avanesyan <avanesyan12345@gmail.com>
2025-02-23 23:36:36 +0400

    Implement sock merchant solution.

---
+++ SalesByMatch/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace SalesByMatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SockMerchant(new List<int> { 10, 20, 20, 10, 10, 30, 50, 10, 20 }));
+        }
+
+        static int SockMerchant(List<int> socks)
+        {
+            if (socks == null || socks.Count == 0) return 0;
+
+            Dictionary<int, int> sockCount = new Dictionary<int, int>();
+            int pairs = 0;
+
+            foreach (int sock in socks)
+            {
+                if (sockCount.ContainsKey(sock))
+                    sockCount[sock]++;
+                else
+                    sockCount[sock] = 1;
+            }
+
+            foreach (var sock in sockCount)
+            {
+                pairs += sock.Value / 2;
+            }
+
+            return pairs;
+        }
+    }
+}
