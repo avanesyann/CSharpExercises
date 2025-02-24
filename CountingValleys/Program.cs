48c87ce047741a269746f776092c249943b5ac6f
Narek Avanesyan <avanesyan12345@gmail.com>
2025-02-24 23:12:56 +0400

    Counting Valleys

---
+++ CountingValleys/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace CountingValleys
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "UDDDUDUU";
+            Console.WriteLine(Valleys(path));
+        }
+
+        static int Valleys(string path)
+        {
+            if (path == null || path.Length == 0)
+                return 0;
+
+            int seaLevel = 0;
+            int countValleys = 0;
+
+            foreach (char step in path)
+            {
+                if (step == 'U')
+                    seaLevel++;
+                else
+                    seaLevel--;
+
+                if (seaLevel == 0 && step == 'U')
+                    countValleys++;
+            }
+            return countValleys;
+        }
+    }
+}
