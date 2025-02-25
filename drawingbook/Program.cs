8db239c752359bf23d1da49672c2741679018275
Narek Avanesyan <avanesyan12345@gmail.com>
2025-02-25 23:30:15 +0400

    Add Drawing Book

---
+++ drawingbook/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace DrawingBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PageCount(5, 4));
+            Console.WriteLine(PageCount(5, 1));
+            Console.WriteLine(PageCount(5, 3));
+        }
+
+        static int PageCount(int n, int p)
+        {
+            if (n < p || p < 1)
+                throw new ArgumentException("Invalid page number.");
+
+            int frontTurns = p / 2;
+            int backTurns = (n / 2) - (p / 2);
+
+            return Math.Min(frontTurns, backTurns);
+        }
+    }
+}
