277d4fd8bd80e8dfddecd94fe6cbf1bbc55fc23e
Narek Avanesyan <avanesyan12345@gmail.com>
2025-03-01 22:30:14 +0400

    Solve Jumping on the Clouds problem.

---
+++ JumpingOnClouds/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Specialized;
+
+namespace JumpingOnClouds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(JumpingOnClouds(new List<int>() { 0, 1, 0, 0, 0, 1, 0 }));
+            Console.WriteLine(JumpingOnClouds(new List<int>() { 0, 0, 0, 0, 1, 0 }));
+        }
+
+        static int JumpingOnClouds(List<int> clouds)
+        {
+            int countJumps = 0;
+            int i = 0;
+
+            while (i < clouds.Count - 1)
+            {
+                if (i + 2 < clouds.Count && clouds[i + 2] == 0)
+                {
+                    i += 2;
+                }
+                else
+                {
+                    i += 1;
+                }
+                countJumps++;
+            }
+            
+
+            return countJumps;
+        }
+    }
+}
