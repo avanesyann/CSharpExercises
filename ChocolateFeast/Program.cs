81a0ffe5a202ee1b13373733ea2f9ebc061d4907
Narek Avanesyan <avanesyan12345@gmail.com>
2025-03-01 19:37:20 +0400

    Solve chocolate feast problem.

---
+++ ChocolateFeast/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ChocolateFeast
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ChocolateFeast(10, 2, 5));    // test 1
+            Console.WriteLine(ChocolateFeast(12, 4, 4));    // test 2
+            Console.WriteLine(ChocolateFeast(6, 2, 2));     // test 3
+        }
+        static int ChocolateFeast(int money, int cost, int exchange)
+        {
+            if (cost > money) return 0;
+
+            int chocolate = money / cost;
+            int wrappers = chocolate;
+
+            while (wrappers >= exchange)
+            {
+                int newChocolate = wrappers / exchange;
+                chocolate += newChocolate;
+                wrappers = (wrappers % exchange) + newChocolate;
+            }
+
+            return chocolate;
+        }
+    }
+}
