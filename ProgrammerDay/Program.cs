b4bbe16f935d4d53d422818af98a87875afdd381
Narek Avanesyan <avanesyan12345@gmail.com>
2025-03-02 11:37:39 +0400

    Solve Day of the Programmer

---
+++ ProgrammerDay/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ProgrammerDay
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DayOfProgrammer(1917));
+            Console.WriteLine(DayOfProgrammer(1918));
+            Console.WriteLine(DayOfProgrammer(1919));
+            Console.WriteLine(DayOfProgrammer(2016));
+        }
+
+        static string DayOfProgrammer(int year)
+        {
+            bool isJulian = (year <= 1917) && (year % 4 == 0);
+            bool isGregorian = (year >= 1919) && ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0));
+
+            int day = (isJulian || isGregorian) ? 12 : 13;
+            if (year == 1918) day = 26;
+
+            return $"{day}.09.{year}";
+        }
+    }
+}
---
+++ ProgrammerDay/README.md
@@ -0,0 +1,9 @@
+# Day of The Programmer 
+
+This program determines the **256th day of a given year** in Russia's history, considering the transition from the Julian to the Gregorian calendar in 1918.
+
+
+## :pushpin: Problem Explanation
+- **Julian Leap Year**: Divisible by `4` (before 1918)
+- **Gregorian Leap Year**: Divisible by `400`, or by `4` but **not** `100` (1919+)
+- **1918 Special Case**: Russia switched from Julian to Gregorian, skipping 13 days
