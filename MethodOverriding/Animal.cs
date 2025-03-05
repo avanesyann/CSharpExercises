6a161f52f115378252fdf4a04132cb5f99ac0519
Narek Avanesyan <avanesyan12345@gmail.com>
2025-03-05 22:41:12 +0400

    Practice some Method Overriding

---
+++ MethodOverriding/Animal.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverriding
+{
+    internal class Cow : Animal
+    {
+        public Cow(string name) : base(name) { }
+        public override void MakeSound()
+        {
+            base.MakeSound();
+            Console.WriteLine("Moo!");
+        }
+    }
+    internal class Cat : Animal
+    {
+        public Cat(string name) : base(name) { }
+        public override void MakeSound()
+        {
+            base.MakeSound();
+            Console.WriteLine("Meow!");
+        }
+    }
+    internal class Dog : Animal
+    {
+        public Dog(string name) : base(name) { }
+        public override void MakeSound()
+        {
+            base.MakeSound();
+            Console.WriteLine("Bark!");
+        }
+    }
+    internal class Animal
+    {
+        private readonly string _name;
+        public Animal(string name)
+        {
+            _name = name;
+        }
+        public virtual void MakeSound()
+        {
+            Console.WriteLine($"{_name} says...");
+        }
+    }
+}
---
+++ MethodOverriding/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace MethodOverriding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>()
+            {
+                new Dog("Jimmy"),
+                new Cat("Tom"),
+                new Dog("Rocky"),
+                new Cow("Flower"),
+                new Cat("Micky")
+            };
+
+            foreach (Animal animal in animals)
+            {
+                animal.MakeSound();
+            }
+        }
+    }
+}
