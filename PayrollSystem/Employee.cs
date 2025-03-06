903e706f7857e8c85e62bcc4725f0a6c39e043c9
Narek Avanesyan <avanesyan12345@gmail.com>
2025-03-06 11:18:17 +0400

    Add PayrollSystem

---
+++ PayrollSystem/Employee.cs
@@ -0,0 +1,46 @@
+﻿namespace PayrollSystem
+{
+    public class FullTime : Employee
+    {
+        public double BaseSalary { get; }
+        public double Bonus { get; }
+
+        public FullTime(string name, double salary, double bonus) : base(name)
+        {
+            BaseSalary = salary;
+            Bonus = bonus;
+        }
+
+        public override double CalculateSalary()
+        {
+            return BaseSalary + Bonus;
+        }
+    }
+    public class PartTime : Employee
+    {
+        public double HourlyRate { get; }
+        public int HoursWorked { get; }
+
+        public PartTime(string name, double hourlyRate, int hoursWorked) : base(name)
+        {
+            HourlyRate = hourlyRate;
+            HoursWorked = hoursWorked;
+        }
+
+        public override double CalculateSalary()
+        {
+            return HourlyRate * HoursWorked;
+        }
+    }
+    public abstract class Employee
+    {
+        public string Name { get; }
+
+
+        public Employee(string name)
+        {
+            Name = name;
+        }
+        public abstract double CalculateSalary();
+    }
+}
---
+++ PayrollSystem/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace PayrollSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>()
+            {
+                new FullTime("Alice", 3000, 500),
+                new FullTime("Bob", 4000, 700),
+                new PartTime("Charlie", 30, 80),
+                new FullTime("Dave", 3500, 400),
+                new PartTime("Jessica", 40, 60),
+                new PartTime("Carl", 40, 90)
+            };
+
+            foreach (Employee employee in employees)
+            {
+                Console.WriteLine($"{employee.Name}'s Salary: {employee.CalculateSalary()}");
+            }
+        }
+    }
+}
