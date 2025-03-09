8c9ca75f47c62ba2ed6aab5670a1c014eeb02594
Narek Avanesyan <avanesyan12345@gmail.com>
2025-03-09 23:30:05 +0400

    Calculate Shape Area

---
+++ ShapeArea/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ShapeArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Shape> shapes = new List<Shape>()
+            {
+                new Circle(4),
+                new Rectangle(5, 10),
+                new Rectangle(6, 12),
+                new Circle(8),
+                new Square(6)
+            };
+
+            foreach (Shape shape in shapes)
+            {
+                shape.DisplayArea();
+            }
+        }
+    }
+}
---
+++ ShapeArea/Shape.cs
@@ -0,0 +1,58 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ShapeArea
+{
+    public class Circle : Shape
+    {
+        public double Radius { get; }
+
+        public Circle(double radius)
+        {
+            if (radius <= 0) throw new ArgumentException("Radius must be greater than zero.");
+            Radius = radius;
+        }
+        public override double CalculateArea()
+        {
+            return Radius * Radius * Math.PI;
+        }
+    }
+    public class Rectangle : Shape
+    {
+        public double Width { get; }
+        public double Length { get; }
+
+        public Rectangle(double width, double length)
+        {
+            if (width <= 0 || length <= 0) throw new ArgumentException("Width and Length must be greater than zero.");
+            Width = width;
+            Length = length;
+        }
+        public override double CalculateArea()
+        {
+            return Width * Length;
+        }
+    }
+    public class Square : Shape
+    {
+        public double Side { get; }
+
+        public Square(double side)
+        {
+            if (side <= 0) throw new ArgumentException("Size must be greater than zero.");
+            Side = side;
+        }
+        public override double CalculateArea()
+        {
+            return Side * Side;
+        }
+    }
+    public abstract class Shape
+    {
+        public abstract double CalculateArea();
+
+        public void DisplayArea()
+        {
+            Console.WriteLine("The area of the shape is: {0:F2}", CalculateArea());
+        }
+    }
+}
