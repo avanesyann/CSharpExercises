1db71f2a634299eb2375628a4df39b54bff0ba33
Narek Avanesyan <avanesyan12345@gmail.com>
2025-03-09 23:10:46 +0400

    Just count the words

---
+++ CamelCase/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace CamelCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CamelCase("justAWord"));
+            Console.WriteLine(CamelCase("someOtherWord"));
+            Console.WriteLine(CamelCase("someQuiteLongWord"));
+            Console.WriteLine(CamelCase("someQuiteReallyLongWord"));
+        }
+
+        static int CamelCase(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return 0;
+
+            int countUppercases = 0;
+            foreach (char c in s)
+            {
+                if (char.IsUpper(c))
+                    countUppercases++;
+            }
+
+            return countUppercases + 1;
+        }
+    }
+}
