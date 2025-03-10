f8043bb379876aa07e321bf665099bbdafefe294
Narek Avanesyan <avanesyan12345@gmail.com>
2025-03-11 00:05:16 +0400

    A Pangram?

---
+++ Pangrams/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Pangrams
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Pangrams("The quick brown fox jumps over the lazy dog."));
+            Console.WriteLine(Pangrams("The quick brown fox."));
+        }
+
+        static string Pangrams(string s)
+        {
+            HashSet<char> characters = new HashSet<char>();
+
+            foreach (char c in s.ToLower())
+            {
+                if (char.IsLetter(c))
+                    characters.Add(c);
+            }
+
+            return characters.Count == 26 ? "pangram" : "not pangram";
+        }
+    }
+}
