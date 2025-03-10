9f17a02cd6240ffdccf5e6f85e22dae05150f460
Narek Avanesyan <avanesyan12345@gmail.com>
2025-03-10 19:42:54 +0400

    Sherlock Holmes

---
+++ SherlockAndSquares/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace SherlockAndSquares
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Squares(3, 9));
+            Console.WriteLine(Squares(35, 84));
+            Console.WriteLine(Squares(17, 48));
+            Console.WriteLine(Squares(121, 1984));
+        }
+
+        static int Squares(int a, int b)
+        {
+            int rootOfA = (int)Math.Ceiling(Math.Sqrt(a));
+            int rootOfB = (int)Math.Floor(Math.Sqrt(b));
+
+            int perfectSquares = rootOfB - rootOfA + 1;
+
+            return perfectSquares;
+        }
+    }
+}
