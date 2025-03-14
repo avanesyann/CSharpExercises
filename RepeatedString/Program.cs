30cc309ba53e96f82fba1474647c53988dc1d62e
Narek Avanesyan <avanesyan12345@gmail.com>
2025-03-14 23:19:07 +0400

    Solve RepeatedString

---
+++ RepeatedString/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace RepeatedString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "a";
+
+            Console.WriteLine(RepeatedString(test, 1000000000000));
+        }
+
+        static long RepeatedString(string s, long n)
+        {
+            long fullReps = n / s.Length;
+            long remaining = n % s.Length;
+
+            int countFullA = s.Count(x => x == 'a');
+            int countRemA = s[..(int)remaining].Count(x => x == 'a');
+
+            return fullReps * countFullA + countRemA;
+        }
+    }
+}
