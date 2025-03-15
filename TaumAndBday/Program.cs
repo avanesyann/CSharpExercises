25d78261be96a89fdc1a9fac16f32162bf4c6dde
Narek Avanesyan <avanesyan12345@gmail.com>
2025-03-15 21:22:30 +0400

    Taum And B'day

---
+++ TaumAndBday/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace TaumAndBday
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TaumBday(3, 5, 3, 4, 1));
+        }
+
+        static long TaumBday(int b, int w, int bc, int wc, int z)
+        {
+            int minBlack = Math.Min(bc, wc + z);
+            int minWhite = Math.Min(wc, bc + z);
+
+            return (b * minBlack) + (w * minWhite);
+        }
+    }
+}
