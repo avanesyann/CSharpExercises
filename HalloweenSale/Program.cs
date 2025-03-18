1698cb03186d6ce8d0c5cb0527b5dc156cbd96a9
Narek Avanesyan <avanesyan12345@gmail.com>
2025-03-18 23:37:02 +0400

    Halloween Sale

---
+++ HalloweenSale/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace HalloweenSale
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(HowManyGames(20, 3, 6, 70));
+        }
+        static int HowManyGames(int firstPrice, int changePrice, int lastPrice, int wallet)
+        {
+            int countGames = 0;
+            int currentPrice = firstPrice;
+
+            while (wallet >= currentPrice)
+            {
+                countGames++;
+                wallet -= currentPrice;
+
+                currentPrice = Math.Max(currentPrice - changePrice, lastPrice);
+            }
+
+            return countGames;
+        }
+    }
+}
---
+++ HalloweenSale/README.md
@@ -0,0 +1,2 @@
+# Halloween Sale 
+The Halloween Sale problem involves a decreasing price structure where you start with an initial price `p`, a discount `d`, and a minimum possible price `m`. You have a given budget `s`, and you need to determine how many games you can buy before running out of money.
