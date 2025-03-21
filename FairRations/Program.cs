73e186ac46d97be990e6331b9047f1c14f61901d
Narek Avanesyan <avanesyan12345@gmail.com>
2025-03-21 12:00:00 +0400

    Backdated commit for yesterday

---
+++ FairRations/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace FairRations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> loaves = new List<int>() { 4, 5, 6, 7 };
+            Console.WriteLine(FairRations(loaves));
+        }
+
+        static string FairRations(List<int> b)
+        {
+            int countLoaves = 0;
+            for (int i = 0; i < b.Count - 1; i++)
+            {
+                if (b[i] % 2 != 0)
+                {
+                    b[i]++;
+                    b[i + 1]++;
+
+                    countLoaves += 2;
+                }
+            }
+
+            int totalLoaves = b.Sum();
+
+            return totalLoaves % 2 == 0 ? countLoaves.ToString() : "NO";
+        }
+    }
+}
