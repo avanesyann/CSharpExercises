83e54f18f53b78ed9740f509ea59bb3c6e123baf
Narek Avanesyan <avanesyan12345@gmail.com>
2025-03-23 17:22:10 +0400

    Solve BeautifulTriplets

--- BeautifulTriplets/Program.cs
+++ BeautifulTriplets/Program.cs
@@ -4,12 +4,21 @@
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine(BeautifulTriplets(1, [2, 2, 3, 4, 5]));
         }
 
         static int BeautifulTriplets(int d, List<int> arr)
         {
+            int count = 0;
+            HashSet<int> numbers = new HashSet<int>(arr);
 
+            foreach (int num in arr)
+            {
+                if (numbers.Contains(num + d) && numbers.Contains(num + 2 * d))
+                    count++;
+            }
+
+            return count;
         }
     }
 }
