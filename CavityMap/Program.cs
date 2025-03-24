d096c72b78e3cb5497af4c4254d6af0d2123c2b2
Narek Avanesyan <avanesyan12345@gmail.com>
2025-03-24 22:15:50 +0400

    Dig a Cavity

---
+++ CavityMap/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace CavityMap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> cavityMap = CavityMap(["1112", "1912", "1892", "1234"]);
+
+            foreach (string row in cavityMap)
+            {
+                Console.WriteLine(row);
+            }
+        }
+
+        static List<string> CavityMap(List<string> grid)
+        {
+            char[][] newGrid = grid.Select(row => row.ToCharArray()).ToArray();
+
+            for (int i = 1; i < grid.Count - 1; i++)
+            {
+                for (int j = 1; j < grid[i].Length - 1; j++)
+                {
+                    char current = grid[i][j];
+
+                    if (current > grid[i - 1][j] && current > grid[i + 1][j] && 
+                        current > grid[i][j - 1] && current > grid[i][j + 1])
+                    {
+                        newGrid[i][j] = 'X';
+                    }
+                }
+            }
+
+            return newGrid.Select(row => new string(row)).ToList();
+        }
+    }
+}
---
+++ CavityMap/README.md
@@ -0,0 +1,3 @@
+# HackerRank CavityMap 
+
+Find all the cavities on the map and replace their depths with the uppercase character **X**.
