9daf23d53b7c4478718d2e3fd832e8205a1ab924
Narek Avanesyan <avanesyan12345@gmail.com>
2025-03-26 23:35:15 +0400

    Strange Counter

---
+++ StrangeCounter/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace StrangeCounter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StrangeCounter(4));
+        }
+
+        static long StrangeCounter(long t)
+        {
+            long cycleStart = 1;
+            long value = 3;
+
+            while (t >= cycleStart + value)
+            {
+                cycleStart += value;
+                value *= 2;
+            }
+
+            return value - (t - cycleStart);
+        }
+    }
+}
