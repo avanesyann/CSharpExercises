3b1692429c9d9b3ffda11f0a8dae3f0c40936bb2
Narek Avanesyan <avanesyan12345@gmail.com>
2025-04-01 22:38:27 +0400

    Add SeparateNumbers exercise files to work on.

---
+++ SeparateNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace SeparateNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SeparateNumbers("91011");
+        }
+
+        static void SeparateNumbers(string s)
+        {
+            List<string> splits = new List<string>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                splits.Add(s[i].ToString());
+            }
+
+            foreach (string splt in splits)
+            {
+                Console.WriteLine(splt);
+            }
+        }
+    }
+}
