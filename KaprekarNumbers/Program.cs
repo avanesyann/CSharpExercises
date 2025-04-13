9a6292e1f41fcf3c5536553eb9755a15eff756c2
Narek Avanesyan <avanesyan12345@gmail.com>
2025-04-13 22:47:50 +0400

    Implement Kaprekar Numbers Solution

--- KaprekarNumbers/Program.cs
+++ KaprekarNumbers/Program.cs
@@ -4,19 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Kaprekar(10, 100);
+            Kaprekar(1, 100);
         }
 
         static void Kaprekar(int p, int q)
         {
+            List<int> kaprekars = new List<int>();
+            int countKaprekar = 0;
+
             for (int i = p; i <= q; i++)
             {
-                string iString = i.ToString();
-                int left = int.Parse(iString.Substring(0, iString.Length / 2));
-                int right = int.Parse(iString.Substring(iString.Length / 2, iString.Length));
+                long square = (long)i * i;
+                string squareStr = square.ToString();
+                int d = i.ToString().Length;
 
-                Console.WriteLine(left + " | " + right);
+                string leftStr = squareStr.Substring(0, squareStr.Length - d);
+                string rightStr = squareStr.Substring(squareStr.Length - d);
+
+                int left = string.IsNullOrEmpty(leftStr) ? 0 : int.Parse(leftStr);
+                int right = int.Parse(rightStr);
+
+                if (i == left + right)
+                {
+                    kaprekars.Add(i);
+                    countKaprekar++;
+                }
             }
+            if (countKaprekar == 0)
+                Console.WriteLine("Invalid Range");
+            else
+                Console.WriteLine(string.Join(" ", kaprekars));
         }
     }
 }
