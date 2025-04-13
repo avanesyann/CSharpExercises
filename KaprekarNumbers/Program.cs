8be063cc7916bbdac27691c0ea906ff79c5a5732
Narek Avanesyan <avanesyan12345@gmail.com>
2025-04-13 17:40:07 +0400

    Push Kaprekar's files

---
+++ KaprekarNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace KaprekarNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kaprekar(10, 100);
+        }
+
+        static void Kaprekar(int p, int q)
+        {
+            for (int i = p; i <= q; i++)
+            {
+                string iString = i.ToString();
+                int left = int.Parse(iString.Substring(0, iString.Length / 2));
+                int right = int.Parse(iString.Substring(iString.Length / 2, iString.Length));
+
+                Console.WriteLine(left + " | " + right);
+            }
+        }
+    }
+}
