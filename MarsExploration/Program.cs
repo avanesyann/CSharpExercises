9942e340e8bd7179509a69cd3315c52a95899908
Narek Avanesyan <avanesyan12345@gmail.com>
2025-04-15 22:00:31 +0400

    Mars Exploration

---
+++ MarsExploration/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace MarsExploration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MarsExploration("SOSSOT"));
+            Console.WriteLine(MarsExploration("SOSSOSSOS"));
+        }
+
+        public static int MarsExploration(string s)
+        {
+            string baseStr = "SOS";
+            int repeatCount = s.Length / baseStr.Length;
+
+            string repeated = string.Concat(Enumerable.Repeat(baseStr, repeatCount));
+
+            int changed = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] != repeated[i])
+                    changed++;
+            }
+
+            return changed;
+        }
+    }
+}
