8efc046e58843502f201cecbd23345003fd6a6d3
Narek Avanesyan <avanesyan12345@gmail.com>
2025-04-16 22:52:39 +0400

    Find the gemstones

---
+++ Gemstones/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Gemstones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> stones = new List<string>()
+            {
+                "abcdde", "baccd", "eeabg"
+            };
+            Console.WriteLine(Gemstones(stones));
+
+        }
+
+        static int Gemstones(List<string> arr)
+        {
+            HashSet<char> stones = new HashSet<char>(arr[0]);
+
+            for (int i = 1; i < arr.Count; i++)
+            {
+                HashSet<char> current = new HashSet<char>(arr[i]);
+                stones.IntersectWith(current);
+            }
+
+            return stones.Count;
+            
+        }
+    }
+}
