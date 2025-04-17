b595511a420e81b4cf7a62c27e98f512361984c9
Narek Avanesyan <avanesyan12345@gmail.com>
2025-04-17 14:42:57 +0400

    Solve SeparateNumbers problem

--- SeparateNumbers/Program.cs
+++ SeparateNumbers/Program.cs
@@ -4,21 +4,38 @@
     {
         static void Main(string[] args)
         {
+            SeparateNumbers("1234");
             SeparateNumbers("91011");
+            SeparateNumbers("99100");
         }
 
         static void SeparateNumbers(string s)
         {
-            List<string> splits = new List<string>();
+            bool found = false;
 
-            for (int i = 0; i < s.Length; i++)
+            for (int i = 1; i <= s.Length / 2; i++)
             {
-                splits.Add(s[i].ToString());
+                long startingPoint = long.Parse(s.Substring(0, i));
+                long current = startingPoint;
+                string sequence = "";
+
+                while (sequence.Length < s.Length)
+                {
+                    sequence += current.ToString();
+                    current++;
+                }
+
+                if (sequence == s)
+                {
+                    Console.WriteLine($"YES {startingPoint}");
+                    found = true;
+                    break;
+                }
             }
 
-            foreach (string splt in splits)
+            if (!found)
             {
-                Console.WriteLine(splt);
+                Console.WriteLine("NO");
             }
         }
     }
