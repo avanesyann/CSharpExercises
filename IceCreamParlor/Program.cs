8e6c054f981aea9f89b34438c951404cf466a51e
Narek Avanesyan <avanesyan12345@gmail.com>
2025-04-19 22:10:17 +0400

    Initial files for IceCreamParlor

---
+++ IceCreamParlor/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace IceCreamParlor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static List<int> IceCreamParlor(int m, List<int> arr)
+        {
+            return new List<int>();
+        }
+    }
+}
