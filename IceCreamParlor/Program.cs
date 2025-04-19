deba86fa00ebd5ffb42c65befda7acfa9f36dc5a
Narek Avanesyan <avanesyan12345@gmail.com>
2025-04-19 22:22:12 +0400

    IceCreamParlor using brute force

--- IceCreamParlor/Program.cs
+++ IceCreamParlor/Program.cs
@@ -4,11 +4,21 @@
     {
         static void Main(string[] args)
         {
-
+            List<int> prices = new List<int>() { 1, 3, 4, 5, 6 };
+            IceCreamParlor(6, prices);
         }
 
         static List<int> IceCreamParlor(int m, List<int> arr)
         {
+            for (int i = 1; i < arr.Count; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (arr[i] + arr[j] == m)
+                        return new List<int>() { j+1, i+1 };
+                }
+            }
+
             return new List<int>();
         }
     }
