fc0868ce1b8d3f5795a1f29d6821b378d06b465e
Narek Avanesyan <avanesyan12345@gmail.com>
2025-04-21 23:03:18 +0400

    Use built-in sorting instead

--- FindMedian/Program.cs
+++ FindMedian/Program.cs
@@ -10,19 +10,7 @@
 
         static int FindMedian(List<int> arr)
         {
-            // insertion sort
-            for (int i = 0; i < arr.Count; i++)
-            {
-                int key = arr[i];
-                int j = i - 1;
-
-                while (j >= 0 && arr[j] > key)
-                {
-                    arr[j + 1] = arr[j];
-                    j = j - 1;
-                }
-                arr[j + 1] = key;
-            }
+            arr.Sort();
 
             return arr[arr.Count / 2];
         }
