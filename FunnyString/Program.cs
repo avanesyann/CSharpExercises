7c98e9ac91773e2775035f35a6cee595285db0c8
Narek Avanesyan <avanesyan12345@gmail.com>
2025-04-23 23:03:29 +0400

    Solve Funny String

---
+++ FunnyString/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace FunnyString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FunnyString("lmnop"));
+        }
+
+        static string FunnyString(string s)
+        {
+            bool isFunny = true;
+
+            char[] straight = s.ToCharArray();
+            char[] reversed = s.ToCharArray();
+            Array.Reverse(reversed);
+
+            for (int i = 1; i < straight.Length; i++)
+            {
+                int originalDiff = Math.Abs(straight[i] - straight[i - 1]);
+                int reversedDiff = Math.Abs(reversed[i] - reversed[i - 1]);
+
+                if (originalDiff != reversedDiff)
+                    return "Not Funny";
+            }
+
+            return "Funny";
+        }
+    }
+}
