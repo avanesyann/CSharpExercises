838f5a23db9d1c084dbc59463bcf0a6be6adc9f0
Narek Avanesyan <avanesyan12345@gmail.com>
2025-04-24 14:11:05 +0400

    Solve Two Strings

---
+++ TwoStrings/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace TwoStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TwoStrings("and", "art"));
+            Console.WriteLine(TwoStrings("be", "cat"));
+        }
+
+        static string TwoStrings(string s1, string s2)
+        {
+            HashSet<char> set1 = new HashSet<char>(s1);
+
+            foreach (char c in s2)
+            {
+                if (set1.Contains(c))
+                    return "YES";
+            }
+            return "NO";
+        }
+    }
+}
