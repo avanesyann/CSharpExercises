1beb640d38d130fcad7a48b3bc24a77c0a5cc000
Narek Avanesyan <avanesyan12345@gmail.com>
2025-04-25 20:37:00 +0400

    Brute Force approach for Two Strings

--- TwoStrings/Program.cs
+++ TwoStrings/Program.cs
@@ -6,6 +6,9 @@
         {
             Console.WriteLine(TwoStrings("and", "art"));
             Console.WriteLine(TwoStrings("be", "cat"));
+
+            Console.WriteLine(TwoStringsBF("and", "art"));
+            Console.WriteLine(TwoStringsBF("be", "cat"));
         }
 
         static string TwoStrings(string s1, string s2)
@@ -17,6 +20,18 @@
                 if (set1.Contains(c))
                     return "YES";
             }
+            return "NO";
+        }
+        static string TwoStringsBF(string s1, string s2)
+        {
+            for (int i = 0; i < s1.Length; i++)
+            {
+                for (int j = 0; j < s2.Length; j++)
+                {
+                    if (s1[i] == s2[j]) return "YES";
+                }
+            }
+
             return "NO";
         }
     }
