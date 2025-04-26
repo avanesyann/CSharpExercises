e0c40aff63e486bcc65af4d143450aea9ae5f248
Narek Avanesyan <avanesyan12345@gmail.com>
2025-04-26 14:17:16 +0400

    Solve Pairs problem

---
+++ Pairs/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Pairs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> testList1 = new List<int>() { 1, 2, 3, 4 };
+            List<int> testList2 = new List<int>() { 5, 2, 6, 8, 3, 10, 4 };
+
+            Console.WriteLine(Pairs(1, testList1));
+            Console.WriteLine(Pairs(6, testList2));
+        }
+
+        static int Pairs(int k, List<int> arr)
+        {
+            int amount = 0;
+            
+            for (int i = 0; i < arr.Count - 1; i++)
+            {
+                for (int j = 1; j < arr.Count; j++)
+                {
+                    if (Math.Abs(arr[i] - arr[j]) == k)
+                        amount++;
+                }
+            }
+
+            return amount;
+        }
+    }
+}
