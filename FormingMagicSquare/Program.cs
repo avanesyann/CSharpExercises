c3ff3e13fc77f2961abd1b6d580d05f7f6735da1
Narek Avanesyan <avanesyan12345@gmail.com>
2025-04-29 23:26:04 +0400

    Add FormingMagicSquares files to work on

---
+++ FormingMagicSquare/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace FormingMagicSquare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static int MagicSquare(List<List<int>> s)
+        {
+
+            return 0;
+        }
+    }
+}
